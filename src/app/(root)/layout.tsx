e9b409474d95794c02e814e6e150d51b2609474e
Karsen H <karsenhansen@gmail.com>
2024-11-21 16:37:11 -0800

    added auth and custom font

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import Navbar from '../components/Navbar';
 
 export default function Layout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
-  return <main>{children}</main>;
+  return (
+    <main>
+      <Navbar />
+      {children}
+    </main>
+  );
 }
