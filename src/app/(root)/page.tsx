3aee2cdf30addf98c7de8fc702fad3bd92a85177
Karsen H <karsenhansen@gmail.com>
2024-11-21 18:51:22 -0800

    created all startups & startup card

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,5 +1,7 @@
 import Image from 'next/image';
 import SearchForm from '@/components/SearchForm';
+import StartupCard from '@/components/StartupCard';
+import { HomeText } from '../../../text';
 
 export default async function Home({
   searchParams,
@@ -7,16 +9,44 @@ export default async function Home({
   searchParams: Promise<{ query: string }>;
 }) {
   const query = (await searchParams)?.query;
+  const posts = [
+    {
+      _createdAt: new Date(),
+      views: 55,
+      author: {
+        _id: 1,
+        name: 'Darren Brown',
+      },
+      _id: 1,
+      description: 'This is a description',
+      image:
+        'https://cdn.pixabay.com/photo/2022/08/15/12/13/robot-7387740_1280.jpg',
+      category: 'Robots',
+      title: 'We Robots',
+    },
+  ];
   return (
     <>
       <section className='pink_container'>
         <h1 className='heading'>
-          pitch your your 💡 <br /> Connect with 💰
+          {HomeText.Header} <br /> {HomeText.SubHeader}
         </h1>
-        <p className='sub-heading'>
-          Submit Ideas, Vote On Pitches, & Connect With Investors.
-        </p>
+        <p className='sub-heading'>{HomeText.Description}</p>
         <SearchForm query={query} />
+      </section>
+      <section className='section_container'>
+        <p className='text-30-semibold'>
+          {query ? `Search results for query "${query}"` : 'All Startups'}
+        </p>
+        <ul className='mt-7 card_grid'>
+          {posts?.length > 0 ? (
+            posts.map((post: StartupCardType, index: number) => (
+              <StartupCard key={post?._id} post={post} />
+            ))
+          ) : (
+            <p className='no-results'>No startups found</p>
+          )}
+        </ul>
       </section>
     </>
   );
---
+++ text.ts
@@ -0,0 +1,5 @@
+export const HomeText = {
+    Header: `Pitch your 💡`,
+    SubHeader: ` Connect with 💰`,
+    Description: `Showcase Your Ideas, Vote On Pitches, & Get Noticed By Investors`
+}
